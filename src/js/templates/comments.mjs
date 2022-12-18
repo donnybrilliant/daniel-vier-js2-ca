e459338b802e01276697f4f72b587dfb2d6d8ae6
donnybrilliant <daniel.vier@gmail.com>
2022-12-18 23:53:23 +0100

    Add display comments on posts

--- src/js/templates/comments.mjs
+++ src/js/templates/comments.mjs
@@ -1,14 +1,30 @@
 import { checkAvatar } from "./avatar.mjs";
 
-// some other method??
+/**
+ * Checks if there are comments and adds HTML if there is
+ * @param {object} data Comments array of object
+ * @returns HTML for each comment
+ */
 
 export function checkComments(data) {
-  let commentString = "";
-  if (data.comments.length > 0) {
-    data.comments.forEach((comment) => {
-      commentString = `${comment.body}`;
-      return commentString;
+  if (data.length > 0) {
+    let comments = "";
+    data.forEach((comment) => {
+      console.log(comment);
+      comments += `<div class="d-flex text-muted pt-3">
+    <a href="/profile/?name=${comment.author}">${checkAvatar(
+        comment.author
+      )}</a>
+
+      <p class="pb-3 mb-0 small lh-sm border-bottom">
+          <strong class="d-block text-gray-dark"><a href="/profile/?name=${
+            comment.author
+          }">${comment.author.name}</a></strong>
+         ${comment.body}
+      </p>
+  </div>`;
     });
+    return comments;
   } else {
     return "No comments";
   }
